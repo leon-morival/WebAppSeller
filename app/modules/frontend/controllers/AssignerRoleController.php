f3ab9ddd31e6ef401185c6533d437707a5782d2c
leon morival <112839274+leoncci@users.noreply.github.com>
2023-10-13 11:58:44 +1100

    Développeur and Chef de projet view and controller deleted

--- app/modules/frontend/controllers/AssignerRoleController.php
+++ app/modules/frontend/controllers/AssignerRoleController.php
@@ -2,20 +2,21 @@
 declare(strict_types=1);
 
 namespace WebAppSeller\Modules\Frontend\Controllers;
-
+use WebAppSeller\Models\Developpeur;
 use WebAppSeller\Models\ChefDeProjet;
 
 
-
-
-class ChefDeProjetController extends ControllerBase
+class AssignerRoleController extends ControllerBase
 {
+    // Your controller code here
     public function indexAction()
     {
-        // Fetch all applications
+        /*Développeur*/
+        $developpeurs = Developpeur::find();
+        $this->view->setVar('developpeurs',$developpeurs);
+
+        /*Chef De Projet*/
         $chefDeProjets = ChefDeProjet::find();
-
-
         $this->view->setVar('chefdeprojets',$chefDeProjets);
     }
 }
